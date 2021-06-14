db5df1209feea200ee5c0097a314f2092e9e1f08
Nil <nilfuzzy@gmail.com>
2021-06-14 11:06:19 +0800

    Refactor code into pipline design pattern

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,52 +1,20 @@
-import urllib.request
-import json
+from yt_concate.pipeline.steps.get_video_list import GetVideoList
+from yt_concate.pipeline.steps.step import StepException
 
-import ssl
+from yt_concate.pipeline.pipeline import Pipeline
 
-
+CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
 
 def main():
-    ssl._create_default_https_context = ssl._create_unverified_context
-    r = urllib.request.urlopen('https://google.com')
-    print(r.status)
-    print(r)
+    inputs = {
+        'channel_id': CHANNEL_ID
+    }
+    steps = [
+        GetVideoList(),
+    ]
 
+    p = Pipeline(steps)
+    p.run(inputs)
 
 if __name__ == '__main__':
     main()
-
-from settings import API_KEY
-print(API_KEY)
-api_key=API_KEY
-CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
-
-
-def get_all_video_in_channel(channel_id):
-
-
-    base_video_url = 'https://www.youtube.com/watch?v='
-    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
-
-    first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(api_key,
-                                                                                                        channel_id)
-
-    video_links = []
-    url = first_url
-    while True:
-        inp = urllib.request.urlopen(url)
-        resp = json.load(inp)
-
-        for i in resp['items']:
-            if i['id']['kind'] == "youtube#video":
-                video_links.append(base_video_url + i['id']['videoId'])
-
-        try:
-            next_page_token = resp['nextPageToken']
-            url = first_url + '&pageToken={}'.format(next_page_token)
-        except:
-            break
-    return video_links
-
-
-video_list = get_all_video_in_channel(CHANNEL_ID)
-print(video_list)
